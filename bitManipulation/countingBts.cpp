59a22414cba6c1bb91f2bbbc45696478f4652054
surya-doc <suryadanabnk@gmail.com>
2021-06-11 11:45:09 +0530

    updated

--- bitManipulation/countingBts.cpp
+++ bitManipulation/countingBts.cpp
@@ -41,18 +41,45 @@ public:
             }
         }
         return ans;
-        // vector<int>num;
-        // for(int i=0; i<=n; i++){
-        //     int temp = i;
-        //     int count = 0;
-        //     while(temp){
-        //         if(temp%2 != 0){
-        //             count++;
-        //         }
-        //         temp = temp >> 1;
-        //     }
-        //     num.push_back(count);
-        // }
-        // return num;
     }
 };
+
+// ********************************************************
+// Optimised solution for <= 10^9
+// See the image for technique better understanding
+
+// You are given a number N. Find the total count of set bits for all numbers from 1 to N(both inclusive).
+
+// Input: N = 4
+// Output: 5
+// Explanation:
+// For numbers from 1 to 4.
+// For 1: 0 0 1 = 1 set bits
+// For 2: 0 1 0 = 1 set bits
+// For 3: 0 1 1 = 2 set bits
+// For 4: 1 0 0 = 1 set bits
+// Therefore, the total set bits is 5.
+
+public:
+int mostPower(int n)
+{
+    int x = 0;
+    while ((1 << x) <= n)
+    {
+        x++;
+    }
+    return (x - 1);
+}
+int countSetBits(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    int x = mostPower(n);
+    int before = x * (1 << (x - 1));
+    int nextCount = n - (1 << x) + 1;
+    int rest = n - (1 << x);
+    int ans = before + nextCount + countSetBits(rest);
+    return ans;
+}
